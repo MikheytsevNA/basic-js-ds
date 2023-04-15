eaf16fbb93b3e2560043bb46ec1efe8eda480448
Nikita Mikheitsev <welt992@gmail.com>
2023-04-15 14:45:34 +0200

    feat: tree and remove

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+//const { NotImplementedError } = require('../extensions/index.js');
 
 // const { Node } = require('../extensions/list-tree.js');
 
@@ -6,43 +6,176 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+class treeNode {
+  prev = null;
+  left = null;
+  right = null;
+  constructor(value) {
+    this.data = value;
+  }
+
+}
+
 class BinarySearchTree {
+  isEmpty = true;
+  first = {
+    prev : null,
+    left : null,
+    right : null,
+    data : 0
+  };
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.isEmpty == true) {
+      return null;
+    } else {
+      return this.first;
+    }
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  choosePath(node, data) {
+    if (data < node.data) {
+      if (node.left == null) {
+        let newNode = new treeNode(data);
+        node.left = newNode;
+        newNode.prev = node.left;
+      }
+        return node.left
+    } else if (data > node.data) {
+      if (node.right == null) {
+        let newNode = new treeNode(data);
+        node.right = newNode;
+        newNode.prev = node.right;
+      }
+      return node.right;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (this.isEmpty == true) {
+      let newNode = new treeNode(data);
+      this.first = newNode;
+      this.isEmpty = false;
+    } else {
+      let currenNode = this.first;
+      while (currenNode != null) {
+        currenNode = this.choosePath(currenNode, data);
+      }
+
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let currenNode = this.first;
+    while (currenNode != null) {
+      if (data < currenNode.data) {
+        currenNode = currenNode.left;
+      } else if (data > currenNode.data){
+        currenNode = currenNode.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let currenNode = this.first;
+    while (currenNode != null) {
+      if (data < currenNode.data) {
+        currenNode = currenNode.left;
+      } else if (data > currenNode.data){
+        currenNode = currenNode.right;
+      } else {
+        return currenNode;
+      }
+    }
+    return null;
+  }
+
+  remove(data) {
+    this.first = this.removeRecursive(this.first, data);
+  }
+
+  removeRecursive (root, data) {
+    if (root == null)
+      return root;
+    if (data < root.data) {
+      root.left = this.removeRecursive(root.left, data);
+    } else if (data > root.data) {
+      root.right = this.removeRecursive(root.right, data);
+    } else {
+      if (root.left == null) {
+        return root.right;
+      } else if (root.right == null) {
+        return root.left;
+      }
+      root.data = this.minFromNode(root.right);
+      root.right = this.removeRecursive(root.right, root.data);
+    }
+    return root;
+  }
+
+  minFromNode (node) {
+    let currenNode = node;
+    while (currenNode != null) {
+      if (currenNode.left == null) {
+        return currenNode.data;
+      } else {
+        currenNode = currenNode.left;
+      }
+    }
+    return node.data;
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let currenNode = this.first;
+    while (currenNode != null) {
+      if (currenNode.left == null) {
+        return currenNode.data;
+      } else {
+        currenNode = currenNode.left;
+      }
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let currenNode = this.first;
+    while (currenNode != null) {
+      if (currenNode.right == null) {
+        return currenNode.data;
+      } else {
+        currenNode = currenNode.right;
+      }
+    }
   }
-}
+
+  inorder() {
+    this.inorderRecursion(this.first);
+  }
+  inorderRecursion(root) {
+    if (root != null) {
+        this.inorderRecursion(root.left);
+        console.log(root.data + " ");
+        this.inorderRecursion(root.right);
+    }
+  }
+  }
+
+let tree = new BinarySearchTree;
+
+/* tree.add(3);
+tree.add(2);
+tree.add(2.5);
+tree.add(1);
+tree.add(4);
+tree.add(3.5);
+tree.add(5);
+tree.inorder();
+console.log(123);
+tree.remove(5);
+tree.inorder(); */
 
 module.exports = {
   BinarySearchTree
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+//const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+//const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,10 +22,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
 }
+
+function removeKFromList(l, k) {       
+  if(l !== null && l.value === k) {
+    if (l.next) {
+      return removeKFromList(l.next, k);
+    } else {
+      return null;
+    }
+  } else {
+    if (l === null) {
+      return null;
+    }
+    let nextNodes = new ListNode;
+    nextNodes.next = removeKFromList(l.next, k);
+    nextNodes.value = l.value;
+    return nextNodes;
+ }
+}
+
+//console.log(removeKFromList(convertArrayToList([3, 1, 2, 3, 4, 5]), 3))
 
 module.exports = {
   removeKFromList
